02059a0a3160758e95967d9d73aa9b79ec7e47eb
etienne <etienne.t.pinard@gmail.com>
2018-06-12 13:16:10 -0400

    first cut vim/

---
+++ vim/README.md
@@ -0,0 +1,11 @@
+# vim
+
+
+## Install neovim
+
+## Dependencies installed on `./setup`
+
+- vim-plug https://github.com/junegunn/vim-plug
+- colorscheme _distinguished_ https://github.com/Lokaltog/vim-distinguished
+
+and see vimrc for list of vim plugins used.
---
+++ vim/setup.sh
@@ -0,0 +1,27 @@
+#! /bin/bash -e
+
+HERE=$(dirname "$(readlink -f "$0")")
+
+# setup directories
+mkdir -p ~/.vim/{colors,autoload}
+mkdir -p ~/.config/nvim/colors
+mkdir -p ~/.local/share/nvim/site/autoload
+
+# link vimrc
+#pushd ~ && ln -fs $HERE/vimrc vimrc && popd
+pushd ~/.config/nvim && ln -fs $HERE/vimrc init.vim && popd
+
+# download and copy vim-plug
+# https://github.com/junegunn/vim-plug
+wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+cp -f plug.vim ~/.vim/autoload
+cp -f plug.vim ~/.local/share/nvim/site/autoload
+
+# download and copy colorschemes
+wget https://raw.githubusercontent.com/Lokaltog/vim-distinguished/develop/colors/distinguished.vim
+cp -f distinguished.vim ~/.vim/colors/distinguished.vim
+cp -f distinguished.vim ~/.config/nvim/colors/distinguished.vim
+
+# install plugins
+vim +PlugIntall +qall
+nvim +PlugIntall +qall
