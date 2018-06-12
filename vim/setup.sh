f16ef62dda843679f13dc0a30562e24bd1825eed
Étienne Tétreault-Pinard <etienne.t.pinard@gmail.com>
2018-06-12 18:14:23 -0400

    more vimrc work

--- vim/setup.sh
+++ vim/setup.sh
@@ -8,7 +8,7 @@ mkdir -p ~/.config/nvim/colors
 mkdir -p ~/.local/share/nvim/site/autoload
 
 # link vimrc
-#pushd ~ && ln -fs $HERE/vimrc vimrc && popd
+#pushd ~ && ln -fs $HERE/vimrc .vimrc && popd
 pushd ~/.config/nvim && ln -fs $HERE/vimrc init.vim && popd
 
 # download and copy vim-plug
@@ -16,11 +16,13 @@ pushd ~/.config/nvim && ln -fs $HERE/vimrc init.vim && popd
 wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp -f plug.vim ~/.vim/autoload
 cp -f plug.vim ~/.local/share/nvim/site/autoload
+rm -f plug.vim
 
 # download and copy colorschemes
 wget https://raw.githubusercontent.com/Lokaltog/vim-distinguished/develop/colors/distinguished.vim
 cp -f distinguished.vim ~/.vim/colors/distinguished.vim
 cp -f distinguished.vim ~/.config/nvim/colors/distinguished.vim
+rm -f distinguished.vim
 
 # install plugins
 vim +PlugIntall +qall
