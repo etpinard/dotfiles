ffb21ae34df1b4978d2695dfb5eae76b9dc0c6cd
Étienne Tétreault-Pinard <etienne.t.pinard@gmail.com>
2018-06-13 22:05:44 -0400

    fixup vim/

--- vim/README.md
+++ vim/README.md
@@ -1,11 +1,8 @@
-# vim
+# vim (and neovim)
 
-
-## Install neovim
-
-## Dependencies installed on `./setup`
+## Dependencies installed on `./_setup.sh`
 
 - vim-plug https://github.com/junegunn/vim-plug
 - colorscheme _distinguished_ https://github.com/Lokaltog/vim-distinguished
 
-and see vimrc for list of vim plugins used.
+and see `vimrc` for list of vim plugins used.
--- vim/_setup.sh
+++ vim/_setup.sh
@@ -2,28 +2,31 @@
 
 HERE=$(dirname "$(readlink -f "$0")")
 
-# setup directories
+echo 'Setup vim/nvim directories'
 mkdir -p ~/.vim/{colors,autoload}
 mkdir -p ~/.config/nvim/colors
 mkdir -p ~/.local/share/nvim/site/autoload
 
-# link vimrc
-#pushd ~ && ln -fs $HERE/vimrc .vimrc && popd
-pushd ~/.config/nvim && ln -fs $HERE/vimrc init.vim && popd
+echo 'Link vimrc'
+#pushd ~ > /dev/null
+#ln -fs $HERE/vimrc .vimrc
+#popd > /dev/null
+pushd ~/.config/nvim > /dev/null
+ln -fs $HERE/vimrc init.vim
+popd > /dev/null
 
-# download and copy vim-plug
-# https://github.com/junegunn/vim-plug
+echo 'Download and copy vim-plug in place'
 wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp -f plug.vim ~/.vim/autoload
 cp -f plug.vim ~/.local/share/nvim/site/autoload
 rm -f plug.vim
 
-# download and copy colorschemes
+echo 'Download and copy distinguished color schemes in place'
 wget https://raw.githubusercontent.com/Lokaltog/vim-distinguished/develop/colors/distinguished.vim
 cp -f distinguished.vim ~/.vim/colors/distinguished.vim
 cp -f distinguished.vim ~/.config/nvim/colors/distinguished.vim
 rm -f distinguished.vim
 
-# install plugins
+echo 'Install plugins as listed in vimrc'
 vim +PlugIntall +qall
 nvim +PlugIntall +qall
