423c4879c33ad9c380bc23b5fbd1def5c601f7f3
Étienne Tétreault-Pinard <etienne.t.pinard@gmail.com>
2018-06-13 22:39:32 -0400

    download bash addons in bash/_setup.sh

--- bash/README.md
+++ bash/README.md
@@ -1,3 +1,8 @@
 # bash
 
 `~/.profile` -> `~/.bashrc` -> `~/.bash_aliases`
+
+## Dependencies installed on `./_setup.sh`
+
+- `z` https://github.com/rupa/z
+- `bash-git-prompt` https://github.com/magicmonty/bash-git-prompt
--- bash/_setup.sh
+++ bash/_setup.sh
@@ -8,3 +8,15 @@ ln -fs $HERE/profile .profile
 ln -fs $HERE/bashrc .bashrc
 ln -fs $HERE/bash_aliases .bash_aliases
 popd > /dev/null
+
+mkdir -p ~/bin
+
+# https://github.com/rupa/z
+echo 'Download rupa/z (jump around) to ~/bin'
+wget https://raw.githubusercontent.com/rupa/z/master/z.sh
+chmod +x z.sh
+mv -f z.sh ~/bin
+
+# https://github.com/magicmonty/bash-git-prompt
+echo 'Download bash-git-prompt to ~/bin'
+git clone https://github.com/magicmonty/bash-git-prompt.git ~/bin/bash-git-prompt --depth=1
