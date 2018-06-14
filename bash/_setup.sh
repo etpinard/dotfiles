dad2321fb4826c07ad7282f1f091341cc83bceb2
Étienne Tétreault-Pinard <etienne.t.pinard@gmail.com>
2018-06-13 22:04:20 -0400

    fixups bash/

--- bash/_setup.sh
+++ bash/_setup.sh
@@ -2,9 +2,9 @@
 
 HERE=$(dirname "$(readlink -f "$0")")
 
-# link profile, bashrc and bash_aliases
-pushd ~
+echo 'Link profile, bashrc and bash_aliases'
+pushd ~ > /dev/null
 ln -fs $HERE/profile .profile
 ln -fs $HERE/bashrc .bashrc
 ln -fs $HERE/bash_aliases .bash_aliases
-popd
+popd > /dev/null
