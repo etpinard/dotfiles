65da36986eb147511b9f8a8a7e79376f9f60ca4d
Étienne Tétreault-Pinard <etienne.t.pinard@gmail.com>
2018-06-13 22:06:08 -0400

    add repo-wide _setup.sh

---
+++ _setup.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+declare -a sub=("bin" "bash" "git" "vim")
+
+for i in "${sub[@]}"; do
+    echo 'Setting up:' $i
+
+    pushd $i > /dev/null
+    chmod +x _setup.sh
+    . _setup.sh
+    popd > /dev/null
+
+    echo ''
+done
