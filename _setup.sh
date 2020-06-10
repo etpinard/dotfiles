57a9cf89ffb26a04a1a52a7ba3a486337bc1c71c
Étienne Tétreault-Pinard <etienne.t.pinard@gmail.com>
2020-06-09 21:25:09 -0400

    improve root _setup.sh

--- _setup.sh
+++ _setup.sh
@@ -1,14 +1,59 @@
 #! /bin/bash
 
-declare -a sub=("bin" "bash" "ssh" "git" "vim" "tmux" "julia" "gnome")
+EXIT_CODE=0
 
-for i in "${sub[@]}"; do
-    echo 'Setting up:' $i
+# all the submodules
+declare -a ALL=("bin" "bash" "ssh" "git" "vim" "tmux" "julia" "gnome")
+# the submodules that should 'always' get installed
+declare -a ALWAYS=("bin" "bash" "ssh" "git" "vim" "tmux")
 
-    pushd $i > /dev/null
-    chmod +x _setup.sh
-    . _setup.sh
-    popd > /dev/null
+_main () {
+    for i in "$@"; do
+        if [[ " ${ALL[@]} " =~ " ${i} " ]]; then
+            echo 'Setting up:' $i
 
-    echo ''
-done
+            pushd $i > /dev/null
+            chmod +x _setup.sh
+            . _setup.sh
+            popd > /dev/null
+
+            echo ''
+        else
+            echo '[ERR] unrecognized submodules ' $i
+            EXIT_CODE=1
+        fi
+    done
+}
+
+_usage () {
+cat << EOF
+    Usage: @etpinard/dotfiles/_setup.sh
+
+    Setup (mostly `ln -s`) personal dotfiles !
+
+    # minimal setup 'required' for every machine I used
+    - ./_setup.sh
+
+    # target specic application (e.g. julia)
+    - ./setup.sh julia
+EOF
+}
+
+case $1 in
+    --help | -h)
+        _usage
+        exit 0
+        ;;
+    ALL | all)
+        _main ${ALL[@]}
+        ;;
+    *)
+        if [ $# -eq 0 ]; then
+            _main ${ALWAYS[@]}
+        else
+            _main $@
+        fi
+        ;;
+esac
+
+exit $EXIT_CODE
